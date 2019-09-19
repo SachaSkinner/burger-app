284b4cba63cfbcf40d7cb3f365add78b8d5c78c8
Alexandra Skinner <aleksandrayandovka@gmail.com>
2019-09-19 12:55:50 -0500

    added models folder and burger.js file with 3 functions

--- config/orm.js
+++ config/orm.js
@@ -8,6 +8,34 @@ var connection = require("connection.js");
 // selectAll()
 // insertOne()
 // updateOne()
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+  var arr = [];
+
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(key + "=" + value);
+    }
+  }
 
 
 // Export the ORM object in module.exports.

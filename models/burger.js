8c59e9d3f27b0171e5798a125c351a45183ea1f5
Alexandra Skinner <aleksandrayandovka@gmail.com>
2019-09-21 17:58:51 -0500

    added heroku db

--- models/burger.js
+++ models/burger.js
@@ -2,18 +2,18 @@ var orm = require("../config/orm.js");
 
 var burger = {
     all: function(cb) {
-      orm.all("burgers", function(res) {
+      orm.all("a53lhvdtnxjq9lqw", function(res) {
         cb(res);
       });
     },
     // The variables cols and vals are arrays.
     create: function(cols, vals, cb) {
-      orm.create("burgers", cols, vals, function(res) {
+      orm.create("a53lhvdtnxjq9lqw", cols, vals, function(res) {
         cb(res);
       });
     },
     update: function(objColVals, condition, cb) {
-      orm.update("burgers", objColVals, condition, function(res) {
+      orm.update("a53lhvdtnxjq9lqw", objColVals, condition, function(res) {
         cb(res);
       });
     },

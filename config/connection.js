20c5acef5372a7a1646849db82f36073c400424f
Alexandra Skinner <aleksandrayandovka@gmail.com>
2019-09-21 14:09:44 -0500

    changed local host for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "password",
+//   database: "burgers_db"
+// });
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
+  user: "ylvmlgqp58fwvb5i",
+  password: "	rl7q933m0eg53rjy",
+  database: "a53lhvdtnxjq9lqw"
 });
 
 // Make connection.
